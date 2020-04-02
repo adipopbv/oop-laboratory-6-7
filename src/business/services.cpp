e93da978070815359c20fa139dfe3f4235c99e89
Adrian Pop <adi.pop.bv@gmail.com>
2020-04-02 14:25:55 +0300

    Optimized service for modifying and deleting from repo

--- src/business/services.cpp
+++ src/business/services.cpp
@@ -41,40 +41,48 @@ void LibraryService::AddBookToRepo(const std::string &title, const std::string &
 
 void LibraryService::ModifyBookInRepo(const std::string &titleSearch, const std::string &authorSearch, const std::string &title, const std::string &author, const std::string &genre, const int &releaseYear)
 {
-	for (int i = 0; i < static_cast<int>(this->booksRepo.Size()); i++) // searching for the book with the given title and author
-	{
-		if (this->booksRepo[i].getTitle() == titleSearch && this->booksRepo[i].getAuthor() == authorSearch) // change book if found
-		{
-			Book newBook = this->booksRepo[i]; // copying the book to be modified
+	// making a working copy of the repo
+	Repo<Book> tempRepo = this->getBooksRepo();
 
-			// changing wanted values
-			if (!title.empty())
-			{ newBook.setTitle(title); }
-			if (!author.empty())
-			{ newBook.setAuthor(author); }
-			if (!genre.empty())
-			{ newBook.setGenre(genre); }
-			if (releaseYear != -1)
-			{ newBook.setReleaseYear(releaseYear); }
+	// getting the book to be modified's index
+	int oldBookIndex = this->getBooksRepo().GetIndexOfElement(
+		[ &titleSearch, &authorSearch ]
+		( Book searchedBook ) 
+		{ return searchedBook.getTitle() == titleSearch && searchedBook.getAuthor() == authorSearch; }
+	);
 
-			this->booksRepo.Erase(i); // remove the old one
-			this->booksRepo.Insert(newBook, i); // insert the new one
+	// making a new one with the wanted values changed
+	Book newBook = tempRepo[oldBookIndex];
+	if (!title.empty())
+	{ newBook.setTitle(title); }
+	if (!author.empty())
+	{ newBook.setAuthor(author); }
+	if (!genre.empty())
+	{ newBook.setGenre(genre); }
+	if (releaseYear != -1)
+	{ newBook.setReleaseYear(releaseYear); }
+	newBook.ValidateData(newBook.getTitle(), newBook.getAuthor(), newBook.getGenre(), newBook.getReleaseYear());
 
-			return;
-		}
-	}
-	throw NotFoundError("book not found in repo"); // exception if book not found
+	// removing the old one and inserting the new one in place
+	tempRepo.Erase(oldBookIndex); // remove the old one
+	tempRepo.Insert(newBook, oldBookIndex); // insert the new one
+	// set the new state
+	this->setBooksRepo(tempRepo);
 }
 
 void LibraryService::DeleteBookFromRepo(const std::string &titleSearch, const std::string &authorSearch)
 {
-	for (int i = 0; i < static_cast<int>(this->booksRepo.Size()); i++) // searching for the book with the given title and author
-	{
-		if (this->booksRepo[i].getTitle() == titleSearch && this->booksRepo[i].getAuthor() == authorSearch) // change book if found
-		{
-			this->booksRepo.Erase(i); // remove the old one
-			return;
-		}
-	}
-	throw NotFoundError("book not found in repo"); // exception if book not found
+	// making a working copy of the repo
+	Repo<Book> tempRepo = this->getBooksRepo();
+
+	// getting the book to be modified's index
+	int oldBookIndex = this->getBooksRepo().GetIndexOfElement(
+		[ &titleSearch, &authorSearch ]
+		( Book searchedBook ) 
+		{ return searchedBook.getTitle() == titleSearch && searchedBook.getAuthor() == authorSearch; }
+	);
+
+	tempRepo.Erase(oldBookIndex); // removing the book
+	// set the new state
+	this->setBooksRepo(tempRepo);
 }
